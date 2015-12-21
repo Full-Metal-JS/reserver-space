b3bba3fae8cb7fb16ba6a0ebbf8577e11f124e09
rcardiff1 <robertstuartcardiff@gmail.com>
2015-12-21 15:01:56 -0800

    edit to h1 tag

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type' : 'text/html'});
 	// Current server does not utilize any html files
-	res.end('<h1 style="text-align: center">roomTap...</h1>');
+	res.end('<h1>roomTap...</h1>');
 });
 
 // Allows the port to either read off the environment variable (e.g. heroku) or set it to 3000
