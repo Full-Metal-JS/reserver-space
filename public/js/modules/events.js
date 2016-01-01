408484fd2ca383ac8e4084753ae962c8eb164557
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-01 14:55:38 -0800

    [update] edit spacing, delete comments, delete console.log

--- public/js/modules/events.js
+++ public/js/modules/events.js
@@ -1,21 +1,13 @@
 angular.module('eventsInfo', ['ngAnimate', 'ui.bootstrap'])
   .controller('eventsController', function($scope, $state, $log, Eventstored) {
-    //4 do we want to save these into an object?
-    $scope.eve = {}
+    $scope.eve = {};
     $scope.eve.eventDate = '';
     $scope.eve.eventToBook = '';
     $scope.eve.eventAlert = '';
     $scope.eve.eTime = '';
-    $scope.eve.roomName = 'kitchen'
-
-
-
-
-    //4 where is our eventSubmit function?
+    $scope.eve.roomName = 'kitchen';
 
     $scope.eventSubmit = function(){
-
-      console.log($scope.eve);
       Eventstored.eventData($scope.eve);
     };
 
@@ -23,17 +15,10 @@ angular.module('eventsInfo', ['ngAnimate', 'ui.bootstrap'])
       $state.go('dashboardPage.events');
     };
 
-
-
-
-
-
     //TIME ADDON
     $scope.eve.eventTime = new Date();
-
     $scope.hstep = 1;
     $scope.mstep = 15;
-
     $scope.options = {
       hstep: [1, 2, 3],
       mstep: [1, 5, 10, 15, 25, 30]
@@ -50,136 +35,100 @@ angular.module('eventsInfo', ['ngAnimate', 'ui.bootstrap'])
       d.setMinutes( 0 );
       $scope.eve.eventTime = d;
     };
-    ///////////END////////////////////
-
-
+    ///////////END/////////////
 
     ////////BUTTONS////////////
-  // $scope.singleModel = 1;
+    // $scope.singleModel = 1;
 
-  // $scope.radioModel = 'Middle';
+    // $scope.radioModel = 'Middle';
 
-  // $scope.checkModel = {
-  //   left: false,
-  //   middle: false,
-  //   right: false
-  // };
+    // $scope.checkModel = {
+    //   left: false,
+    //   middle: false,
+    //   right: false
+    // };
 
-  
+    $scope.$watchCollection('checkModel', function () {
+      $scope.checkResults = [];
 
-
-  $scope.$watchCollection('checkModel', function () {
-    $scope.checkResults = [];
-
-    angular.forEach($scope.checkModel, function (value, key) {
-      if (value) {
-        $scope.checkResults.push(key);
-        console.log('inside res:', $scope.checkResults)
-
-      }
+      angular.forEach($scope.checkModel, function (value, key) {
+        if (value) {
+          $scope.checkResults.push(key);
+        }
+      });
     });
-  });
 
-  $scope.eve.roomName = [];
-  $scope.$watchCollection('checkResults', function(){
-    console.log('this fired!');
-    $scope.eve.roomName = $scope.checkResults;
-  })
+    $scope.eve.roomName = [];
+    $scope.$watchCollection('checkResults', function(){
+      console.log('this fired!');
+      $scope.eve.roomName = $scope.checkResults;
+    });
+    ////////////////////////////////
 
-  ////////////////////////////////
+    $scope.today = function() {
+    $scope.eve.eventDate = new Date();
+    };
+    $scope.today();
 
+    $scope.clear = function () {
+      $scope.eve.eventDate = null;
+    };
 
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
 
-  $scope.today = function() {
-  $scope.eve.eventDate = new Date();
-  };
-  $scope.today();
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+    $scope.maxDate = new Date(2020, 5, 22);
 
-  $scope.clear = function () {
-    $scope.eve.eventDate = null;
-  };
+    $scope.open = function($event) {
+      $scope.status.opened = true;
+    };
 
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
+    $scope.setDate = function(year, month, day) {
+      $scope.eve.eventDate = new Date(year, month, day);
+    };
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
-  $scope.maxDate = new Date(2020, 5, 22);
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
 
-  $scope.open = function($event) {
-    $scope.status.opened = true;
-  };
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
 
-  $scope.setDate = function(year, month, day) {
-    $scope.eve.eventDate = new Date(year, month, day);
-  };
+    $scope.status = {
+      opened: false
+    };
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    var afterTomorrow = new Date();
+    afterTomorrow.setDate(tomorrow.getDate() + 2);
+    $scope.events =[{
+          date: tomorrow,
+          status: 'full'
+        },
+        {
+          date: afterTomorrow,
+          status: 'partially'
+        }
+      ];
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
-
-  $scope.status = {
-    opened: false
-  };
-
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 2);
-  $scope.events =
-    [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
-
-  $scope.getDayClass = function(date, mode) {
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-      for (var i=0;i<$scope.events.length;i++){
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
+    $scope.getDayClass = function(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+        for (var i=0;i<$scope.events.length;i++){
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
+          }
         }
       }
-    }
-
-    return '';
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      return '';
+    };
   });
