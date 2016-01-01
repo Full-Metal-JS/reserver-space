2727c79b8d917d31119b1cc879c6747ff8e39b7f
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-01 14:54:57 -0800

    [update] edit comments

--- public/js/modules/room.js
+++
@@ -1,9 +0,0 @@
-// angular.module('signUp', [])
-//     .controller('ExampleController', ['$scope', function($scope) {
-//       $scope.room = {
-//         name: 'kitchen'
-//       };
-// }]);
-
-
-//what is the point of the this file?
