a08ad5868fb221be1da77859e6c9e6a3fc0b5227
peterkaiserui <peterkaiserui@gmail.com>
2016-01-06 11:27:08 -0800

    basic model displaying

--- public/assets/js/modules/landing.js
+++ public/assets/js/modules/landing.js
@@ -1,3 +1,49 @@
-angular.module('landing', [])
-  .controller('LandingController', ['$scope', function ($scope) {
-  }]);
+angular.module('landing', ['ngAnimate', 'ui.bootstrap'])
+  .controller('LandingController', function ($scope, $uibModal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+  	
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+});
+
+angular.module('ui.bootstrap').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $uibModalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
