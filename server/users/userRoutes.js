3f57db5593596815d9ae54080d0e3e90a12b381a
Elliot Chi <elliotschi@gmail.com>
2016-01-06 16:16:12 -0800

    [chore]: delete unused files in file structure

--- server/users/userRoutes.js
+++
@@ -1,6 +0,0 @@
-var userController = require('./userController.js');
-
-module.exports = function(app) {
-  app.post('/signup', userController.signup);
-  app.post('/login', userController.login);
-};
