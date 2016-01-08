953aed10042fac26fc2fd08b244b7a86b1c98883
Elliot Chi <elliotschi@gmail.com>
2016-01-07 19:40:11 -0800

    [feat]: returns promise to get all data

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -3,10 +3,7 @@ var _ = require('underscore');
 var Promise = require('bluebird');
 
 module.exports = {
-  getAllData: function(user, callback) {
-    models.sequelize.query('select * from "Locations" left join "Rooms" on "Locations".id="Rooms"."LocationId" left join "Reservations" on "Rooms".id = "Reservations"."RoomId";')
-      .then(function(result) {
-        return result;
-      })
+  getAllData: function(user) {
+    return models.sequelize.query('select * from "Locations" left join "Rooms" on "Locations".id="Rooms"."LocationId" left join "Reservations" on "Rooms".id = "Reservations"."RoomId";');
   }
 }
