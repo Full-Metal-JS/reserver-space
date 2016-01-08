e5d42b743804fcff3ef487f1f056236f144f23f0
Ian Bari <ian.bari@att.net>
2016-01-07 19:44:44 -0800

    [feat]: include add location bar

--- public/assets/js/modules/dashboard.js
+++ public/assets/js/modules/dashboard.js
@@ -1,6 +1,27 @@
 angular.module('dashboard', [])
   .controller('DashboardController', function($scope, UserFactory) {
-    $scope.locations = UserFactory.currentUser.data.locations;
+    $scope.locations = (UserFactory.currentUser.data.locations !== undefined) ? 
+                        UserFactory.currentUser.data.locations : [];
+
+    $scope.addbar = {
+      text: '',
+      placeholder: 'Add location'
+    };
+
+    $scope.addLocation = function() {
+      UserFactory.addLocation($scope.addbar.text)
+        .then(function(location) {
+          console.log('location: ', location);
+
+          $scope.locations.push(location);
+          $scope.addbar.text = '';
+          //$scope.alerts.push({type: 'success', msg: 'Your preferences have been saved!'});
+        })
+        .catch(function(error) {
+          //$scope.alerts.push({type: 'danger', msg: 'Error saving preferences.'});
+          console.error('Error adding location: ', error);  
+        });
+    };
   });
 
 
