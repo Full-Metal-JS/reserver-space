8537ae86f2206ce84540c768f7ab69e5cdc9a97c
Elliot Chi <elliotschi@gmail.com>
2016-04-10 19:37:19 -0700

    feat: add passport middleware file

---
+++ server/passport.js
@@ -0,0 +1,62 @@
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+
+const applyPassportMiddleware = (app, passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
+  });
+  
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, (req, email, password, done) => {
+    // logic of signup
+  })
+  );
+  
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true   
+  }, (req, email, password, done) => {
+    process.nextTick(() => {
+      // logic of signin
+    });
+  }));
+  
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileFields: ['id', 'displayName', 'email', 'photos', 'timezone', 'gender']
+  },
+    (token, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        done(null, profile);
+      });
+    }
+  ));
+  
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_APP_ID,
+    clientSecret: process.env.GOOGLE_APP_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  }, 
+    (token, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        done(null, profile)
+      });
+    }
+  ));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
+
+module.exports = applyPassportMiddleware;
