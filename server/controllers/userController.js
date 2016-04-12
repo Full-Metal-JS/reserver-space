343e59ba84a3769fd7fc02e1a197d63a21d4c0bc
Elliot Chi <elliotschi@gmail.com>
2016-04-11 19:37:14 -0700

    feat: add user delete and user put route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,37 @@
 'use strict'
 
 const db = require('./../db/db');
+const User = require('./../db/models/userModel');
+const _ = require('lodash');
 
 const userController = {
+  // update user 
   userPut: (req, res, next) => {
-    
+    if (!_.isEmpty(req.body && req.params.userId)) {
+      User.updateUser(req.params.userId, req.body)
+        .then(updatedUser => {
+          res.json(updateUser);
+        })
+        .catch(err => {
+          next(err);
+        });
+    } else {
+      next(new Error('must send update params and userId'));
+    }
+  },
+  
+  userDelete: (req, res, next) => {
+    if (req.params.userId) {
+      User.deleteUser(userId)
+        .then(deletedUser => {
+          res.json(deletedUser);
+        })
+        .catch(err => {
+          next(err);
+        });
+    } else {
+      next(new Error('must send user id'));
+    }
   }
 }
 
