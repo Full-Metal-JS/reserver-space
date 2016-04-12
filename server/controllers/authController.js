a8b2c8fcfc1ffd64f38e34c0cd2c58ae0c305c00
Elliot Chi <elliotschi@gmail.com>
2016-04-12 14:04:52 -0700

    feat: add facebook and google

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -24,7 +24,66 @@ const authController = {
     console.log('passport', req.passport)
     console.log('user', req.user);
     res.send('success');
-  }
+  },
+  
+  validateGoogle: (req, res, next) => {
+    let googleId = req.user.id;
+    
+    req.session.googleId = googleId;
+    req.session.picture = req.user._json.image.url + '0';
+    req.session.email = req.user.emails[0].value;
+    
+    User.getUserByParameter('googleId', googleId)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        User.createUser('google', googleId)
+          then(createdUser => {
+            res.json(createdUser);
+          })
+          .catch(err => {
+            next(err);
+          });
+      });
+  },
+  
+  validateFacebook: (req, res, next) => {
+    req.session.name = req.user.displayName;
+    req.session.facebookId = req.user.id;
+    req.session.picture = `https://graph.facebook.com/${req.user.id}/picture?height=500`;
+    req.session.email = req.user.emails[0].value;
+    
+    User.getUserByParameter('facebook', req.user.id)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        User.createUser('facebook', req.user.id)
+          .then(createdUser => {
+            res.json(createdUser);
+          })
+          .catch(err => {
+            next(err);
+          });
+      });
+  },
+  
+  facebook: passport.authenticate('facebook', {
+    scope: ['email']
+  }),
+
+  facebookCallback: passport.authenticate('facebook', {
+    failureRedirect: '/'
+  }),
+
+  google: passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }),
+
+  googleCallback: passport.authenticate('google', {
+    failureRedirect: '/login'
+  })
 };
 
 module.exports = authController;
