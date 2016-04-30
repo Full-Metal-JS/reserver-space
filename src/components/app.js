61259b404172d19c4d556b05e0100f9e75e208b6
seanagibson <seanandgibson@gmail.com>
2016-04-29 20:18:05 -0700

    refac: adds routes and removes unused files

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import DevTools from './devTools';
 
 export default class App extends Component {
-  
+
   render() {
+    let devTools = process.env.NODE_ENV === 'development' ? <DevTools/> : '';
+    
     return (
       <div>
-        Reserver Space
+        {this.props.children}
+        {devTools}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import {Router, Route, IndexRoute, Redirect, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
 
 import App from './components/app';
+import HomeContainer from './containers/homeContainer';
 
-import reducers from './reducers/index';
-
-const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+const store = configureStore(browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App/>
+    <Router history={history}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home}>
+          <IndexRoute path='login' component={loginContainer}></IndexRoute>
+          <Route path='signup' component={signupContainer}></Route>
+        </IndexRoute>
+      </Route>
+      <Redirect from='*' to='/' />
+    </Router>
   </Provider>
-  , document.querySelector('.container')
-);
+  , document.getElementById('app'));
