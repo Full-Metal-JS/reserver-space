7f3b50ff728548832efb9236fa4a8f2facdf75de
seanagibson <seanandgibson@gmail.com>
2016-04-29 17:23:32 -0700

    feat: adds actions for user auth

---
+++ src/actions/actionConstants.js
@@ -0,0 +1,14 @@
+//---------Auth Action Constants--------------
+export const ADD_EMAIL = 'ADD_EMAIL';
+export const ADD_PASSWORD = 'ADD_PASSWORD';
+export const LOGIN_SUBMIT = 'LOGIN_SUBMIT';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const SIGNUP_FIRST_NAME = 'SIGNUP_FIRST_NAME';
+export const SIGNUP_LAST_NAME = 'SIGNUP_LAST_NAME';
+export const SIGNUP_SUBMIT = 'SIGNUP_SUBMIT';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const LOGOUT_SUBMIT = 'LOGOUT_SUBMIT';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
---
+++ src/actions/authActions.js
@@ -0,0 +1,161 @@
+import * as actions from './actionConstants';
+
+export const addEmail = (email) => {
+  return {
+    type: actions.LOGIN_EMAIL,
+    email
+  }
+};
+
+export const addPassword = (password) => {
+  return {
+    type: actions.LOGIN_PASSWORD,
+    password
+  }
+};
+
+export const submitLogin = () => {
+  return {
+    type: actions.LOGIN_SUBMIT
+  }
+}
+
+export const loginSuccess = (user) => {
+  return {
+    type: actions.LOGIN_SUCCESS,
+    user
+  }
+};
+
+export const loginFailure = (error) => {
+  return {
+    type: actions.LOGIN_FAILURE,
+    error
+  }
+};
+
+export const postLogin = (user) => {
+  return (dispatch, getState) => {
+    let state = getState();
+    if(!state.postingLogin){
+      dispatch(submitLogin());
+      fetch('/auth/local-login', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300){
+          dispatch(loginSuccess(response.user));
+        }
+        else {
+          let error = new Error(response.statusText);
+          dispatch(loginFailure(error));
+        }
+      })
+    }
+  }
+}
+
+export const addSignupFirstName = (firstName) => {
+  return {
+    type: actions.SIGNUP_FIRST_NAME,
+    firstName
+  }
+};
+
+export const addSignupLastName = (lastName) => {
+  return {
+    type: actions.SIGNUP_LAST_NAME,
+    lastName
+  }
+};
+
+export const submitSignup = () => {
+  return {
+    type: actions.SIGNUP_SUBMIT
+  }
+};
+
+export const signupSuccess = () => {
+  return {
+    type: actions.SIGNUP_SUCCESS
+  }
+};
+
+export const signupFailure = (error) => {
+  return {
+    type: actions.SIGNUP_FAILURE,
+    error
+  }
+};
+
+export const postSignup = (user) => {
+  return (dispatch, getState) => {
+    let state = getState();
+    if(!state.postingSignup){
+      dispatch(submitSignup());
+      fetch('/auth/local-signup', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300){
+          dispatch(signupSuccess());
+        }
+        else {
+          let error = new Error(response.statusText);
+          dispatch(signupFailure(error));
+        }
+      })
+    }
+  }
+}
+
+export const submitLogout = () => {
+  return {
+    type: actions.LOGOUT_SUBMIT
+  }
+};
+
+export const logoutSuccess = () => {
+  return {
+    type: actions.LOGOUT_SUCCESS
+  }
+};
+
+export const logoutFailure = (error) => {
+  return {
+    type: actions.LOGOUT_FAILURE,
+    error
+  }
+}
+
+export const postLogout = () => {
+  return (dispatch) => {
+    dispatch(submitLogout());
+    fetch('/auth/logout', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300){
+        dispatch(logoutSuccess());
+      }
+      else {
+        let error = new Error(response.statusText);
+        dispatch(logoutFailure(error));
+      }
+    })
+  }
+}
