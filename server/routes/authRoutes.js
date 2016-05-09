36616554c191c01be486500e27a966c18c8a0c1a
Elliot Chi <elliotschi@gmail.com>
2016-05-09 16:49:05 -0700

    feat: add passport fb route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,22 @@
-const Auth = require('./../controllers/authController');
+const { signup, login, logout, updateSession,
+  facebook, facebookCallback, validateFacebook, 
+  google, googleCallback, validateGoogle
+} = require('./../controllers/authController');
 
 const authRouteConfig = (router) => {
-  router.post('/signup', Auth.signup, Auth.updateSession);
+  router.post('/signup', signup, updateSession);
   
-  router.post('/login', Auth.login, Auth.updateSession);
+  router.post('/login', login, updateSession);
   
-  router.get('/logout', Auth.logout);
+  router.get('/logout', logout);
+
+  router.get('/facebook', facebook);
+
+  router.get('/facebook/callback', facebookCallback, validateFacebook);
+
+  router.get('/google', google);
+
+  router.get('/google/callback', googleCallback, validateGoogle);
 };
 
 module.exports = authRouteConfig;
