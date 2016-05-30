7f0a34f4e06eb5b92a12e6690adeb15ca0c7e904
Elliot Chi <elliotschi@gmail.com>
2016-05-29 22:02:20 -0700

    refactor: change some syntax

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import SidebarLocation from './SidebarLocation';
 
 const Sidebar = ({locationsList, roomsList}) => {
-  let locations = locationsList ? locationsList.map(function(location){
-    return <SidebarLocation location={location} roomsList={roomsList}/>}) : '';
+  let locations = locationsList ? locationsList.map(location => 
+    <SidebarLocation location={location} roomsList={roomsList}/>) : '';
 
   return (
     <div className='col-sm-3 col-md-2 sidebar'>
--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -5,6 +5,35 @@ import * as actions from './../actions/actionConstants';
   location with numbered keys as its id, and each location is an object with a rooms
   property which is a map, which has numbered keys as its id and each room is an object with a reservations property which is a map. 
 */
+const initialLocationState = {
+  
+};
+
+const locations = (state = initialLocationState, action) => {
+  switch(action.type) {
+    case actions.GET_USERS_LOCATIONS:
+     return {
+       ...state
+     };
+  }
+};
+
+const initialRoomState = {
+  
+}
+
+const rooms = (state = initialRoomState, action) => {
+  
+};
+
+const initialReservationState = {
+  
+};
+
+const reservations = (state = initialReservationState, action) => {
+  
+};
+
 const initialState = {
   isFetching: false,
   error: '',
@@ -25,6 +54,12 @@ export default (state = initialState, action) => {
         ...state,
         isFetching: false,
         error: action.error
+      }
+      
+    case actions.FETCHING_DATA_SUCCESS:
+      return {
+        ...state,
+        locations
       }
   }
 }
