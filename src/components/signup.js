fc49d3b5fefc3a1dfb8194150448db0f2a0af7f5
seanagibson <seanandgibson@gmail.com>
2016-06-05 21:51:51 -0700

    refac: Adds styles to login, signup, and sidebar components

--- src/components/signup.js
+++ src/components/signup.js
@@ -12,32 +12,36 @@ const Signup = ({addFirstName, addLastName, addEmail, addPassword, postSignup, u
       password: userPassword
     });
   };
-  
+
   return (
-    <div className='container authContainer'>
-        <div className='text-center'>
-          <h2>Sign Up</h2>
-          <h3><Link to={'/login'}>Log in here</Link></h3>
+    <div className='container authContainer text-center'>
+      <h2 className='auth'>Sign Up</h2>
+      <div>
+        <div className='facebook-auth'>
+          <a href='/auth/facebook' className='btn btn-primary btn-facebook'><i className='fa fa-facebook-official'></i>Sign up with Facebook</a>
         </div>
-        <div>
-          <div className='facebook-auth'>
-            <a href='/auth/facebook' className='btn btn-primary btn-facebook'>Sign up with Facebook</a>
-          </div>
-          <div className='google-auth'>
-            <a href='/auth/google' className='btn btn-primary btn-google'>Sign up with Google</a>
-          </div>
+        <div className='google-auth'>
+          <a href='/auth/google' className='btn btn-primary btn-google'><i className='fa fa-google'></i>Sign up with Google</a>
         </div>
+      </div>
       <form>
-        <h3>Or sign up with email</h3>
-        <div className='col-sm-12'>
-          <input type='email' placeholder='Your Email' onBlur={(e) => {addEmail(e.target.value)}} />
+        <p>Or sign up with email</p>
+        <div className='authInput'>
+          <input id='nameInput' type='text' placeholder='First Name' onBlur={(e) => {addSignupFirstName(e.target.value)}}/>
+          <input id='nameInput' type='text' placeholder='Last Name' onBlur={(e) => {addSignupLastName(e.target.value)}}/>
         </div>
-        <div className='col-sm-12'>
+        <div className='authInput'>
+          <input type='email' placeholder='Email' onBlur={(e) => {addEmail(e.target.value)}} />
+        </div>
+        <div className='authInput'>
           <input type='password' placeholder='Password' onBlur={(e) => {addPassword(e.target.value)}} />
         </div>
-        <button className='btn btn-primary btn-block'
+        <button className='btn btn-primary btn-auth'
           onClick={handleSubmit.bind(this)}>Sign Up</button>
       </form>
+      <div className='authLink'>
+        <h4><Link to={'/login'}>Log in here</Link></h4>
+      </div>
     </div>
   );
 };
